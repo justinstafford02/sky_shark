903c668e1843e18fc66cc4f5fbb294428f76a848
justinstafford02 <duckyabrams@gmail.com>
2023-03-14 01:27:52 -0700

    initial commit

---
+++ Day1/welcome.java
@@ -0,0 +1,11 @@
+/**
+ * welcome
+ */
+public class welcome {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Justin");
+        System.out.println("My ideal salary is:" + 115000);
+
+    }
+}
